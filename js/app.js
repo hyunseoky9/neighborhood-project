958699a73fa404a12bc5a0ce7782edd2338c5279
jin00105 <hy324@cornell.edu>
2017-09-18 02:29:45 -0700

    addded info window

--- js/app.js
+++ js/app.js
@@ -12,18 +12,10 @@ function initMap() {
     title: 'First Marker!',
     animation: google.maps.Animation.DROP
   });
+  var infowindow = new google.maps.InfoWindow({
+      content: 'What the fuck Martha you little shit'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 }
-/*var map;
-function initMap() {
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13
-  });
-  var tribeca = {lat: 40.719526, lng: -74.0089934};
-  var marker = new google.maps.Marker({
-    position: tribeca,
-    map: map,
-    title: 'First Marker!'
-  });
-}*/
